188897b5d5e86c1d73103a7d1b694fd6d72d1628
takaneo <takaneo@gmail.com>
2023-04-11 00:49:16 +0900

    課題1_22

---
+++ javakarikyuramu/src/lesson1_22/lesson1_22.java
@@ -0,0 +1,19 @@
+package lesson1_22;
+
+public class lesson1_22 {
+	
+	public String greeting ; 
+	public String food ;
+	public String meal;
+	
+	public String day ;
+	
+//	thiを使う
+	public lesson1_22 (String greeting,String food,String meal,String day) {
+		this.greeting = greeting;
+		this.food = food;
+		this.meal = meal;
+		this.day = day;
+	}
+
+}
