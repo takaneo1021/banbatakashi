51ef0ec4199634f0b6db09f5e3cc5d66174bebc0
takaneo <takaneo@gmail.com>
2023-04-15 10:07:40 +0900

    課題1.19更新

--- javakarikyuramu/src/lesson1_19/lesson1_19_1.java
+++ javakarikyuramu/src/lesson1_19/lesson1_19_1.java
@@ -8,21 +8,21 @@ import java.util.Date;
 		public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 				
-	     //Dogを呼び出し
+	    // Dogを呼び出し
 			Dog sub = new Dog();
-	     //メソッドの呼び出し
+	    // メソッドの呼び出し
 			sub.publicMethod();
 			
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
 			
-	     //DOGのクラスからnumberを取得。
+	    // DOGのクラスからnumberを取得。
 			String num = sub.number;
 			System.out.println(num);	
 			
 			
-	     // 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+	    // 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 			
-	     //日付をコントロールに出力。
+	    // 日付をコントロールに出力。
 			Date date = new Date();
 			SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd  HH:mm:ss");
 			System.out.println(df.format(date));
@@ -30,12 +30,12 @@ import java.util.Date;
 		}
 	
 	}
-//         Dogのクラス作成
+         // Dogのクラス作成
 	class Dog {	
-//		   Q2の数の呼び出し。
+         // Q2の数の呼び出し。
 		    public String number = "3";
 		    
-//		   Q1の名前の呼び出し。
+         // Q1の名前の呼び出し。
 		    public void publicMethod() {
 			System.out.println("シマウマ");
 			
--- javakarikyuramu/src/lesson1_19/lesson1_19_2.java
+++ javakarikyuramu/src/lesson1_19/lesson1_19_2.java
@@ -11,11 +11,11 @@ package lesson1_19;
 	
 			// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
 	
-			//		Q1のクラス作成
+			// Q1のクラス作成
 			Natural na = new Natural();
 			System.out.println(na.bird);
 	
-			//		Q2のクラス作成
+			// Q2のクラス作成
 			Earth ea = new Earth(3);
 			ea.assignment();
 	
@@ -24,30 +24,30 @@ package lesson1_19;
 		}
 	}
 	
-	//  クラス作成Q1
+	        // クラス作成Q1
 	class Natural{
 	
-	   // フィールド作成
+	        // フィールド作成
 		   String bird ;
 	
-		//		Q3 コントラクト作成
+		    // Q3 コントラクト作成
 		   public Natural(){        
 			bird = "犬" ;
 		}
 	}
 	
-	//	 Q2 クラス作成
+	        // Q2 クラス作成
 	class Earth{
 	
-		//		 フィールド作成
+		    // フィールド作成
 		   private int hawk ;
 	
-		//		 Q4の引数を代入
+		    // Q4の引数を代入
 		   Earth(int newPlace){
 			  hawk = newPlace;
 		}
 	
-		//		 Q4 コントラクト
+		    // Q4 コントラクト
 		    public void assignment() {
 			System.out.println(hawk);
 		}
