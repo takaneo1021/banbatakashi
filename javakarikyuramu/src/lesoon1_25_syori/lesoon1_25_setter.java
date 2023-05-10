4c5d69ee2a237dbc46dfe9ce204b8d800b973a8e
takaneo <takaneo@gmail.com>
2023-05-10 10:00:49 +0900

    課題　1.25　バックアップ

--- javakarikyuramu/src/lesoon1_25_syori/lesoon1_25_setter.java
+++
@@ -1,9 +0,0 @@
-package lesoon1_25_syori;
-
-public class lesoon1_25_setter extends lesoon1_25_syori {
-
-	public lesoon1_25_setter(String 名前) {
-		super();
-		set名前(名前);
-	}
-}
--- javakarikyuramu/src/lesoon1_25_syori/lesoon1_25_syori.java
+++ javakarikyuramu/src/lesoon1_25_syori/lesoon1_25_syori.java
@@ -1,59 +1,39 @@
 package lesoon1_25_syori;
 
-import java.util.Random;
-
 public class lesoon1_25_syori {
 	
-	private String 名前;
+	private String name;
 	private int HP;
 	private int MP;
 	private int 攻撃力;
 	private int 素早さ;
 	private int 防御力;
 	
-	public lesoon1_25_syori() {
-		Random rand = new Random();
-		HP = rand.nextInt();
-		MP = rand.nextInt();
-		攻撃力 = rand.nextInt();
-		素早さ = rand.nextInt();
-		防御力 = rand.nextInt();
-		}
-	
-	public String get名前() {
-		return 名前;
+	public String getName() {
+		return name;
 		}
 
-		public void set名前(String 名前) {
-		this.名前 = 名前;
+		public void setName(String name) {
+		this.name = name;
 		}
 		
-		@Override
-		public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("こんにちは 「");
-		sb.append(get名前());
-		sb.append(" 」 さん");
-		sb.append("ステータス");
-		sb.append(System.lineSeparator());
-		sb.append("HP：");
-		sb.append(HP);
-		sb.append(System.lineSeparator());
-		sb.append("MP：");
-		sb.append(MP);
-		sb.append(System.lineSeparator());
-		sb.append("攻撃力：");
-		sb.append(攻撃力);
-		sb.append(System.lineSeparator());
-		sb.append("素早さ：");
-		sb.append(素早さ);
-		sb.append(System.lineSeparator());
-		sb.append("防御力：");
-		sb.append(防御力);
-		sb.append(System.lineSeparator());
-		sb.append(System.lineSeparator());
-		sb.append("さあ冒険に出かけよう！");
-		return sb.toString();
+		public void status(String name) {
+			System.out.println("こんにちは 「" + this.name + "」 さん");
+			System.out.println("ステータス");
 		}
+		
+		public class lesoon1_25_setter extends lesoon1_25_syori{
 
-}
+			public void status(String name){
+				super.setName(name);
+				
+				System.out.println("HP:" + HP);
+				System.out.println("MP:" + MP);
+				System.out.println("攻撃力:" + 攻撃力);
+				System.out.println("素早さ:" + 素早さ);
+				System.out.println("防御力:" + 防御力);
+				System.out.println();
+				System.out.println("さあ冒険に出かけよう！");
+		}
+		} 
+	}
